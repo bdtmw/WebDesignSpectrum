6783b39eab64e63b9301933ae36d501b1af3c05b
bdtmw <bdtmw50@gmail.com>
2026-08-11 22:22:03 +0500

    Update PackageForm.jsx

--- src/components/Forms/PackageForm.jsx
+++ src/components/Forms/PackageForm.jsx
@@ -5,7 +5,7 @@ import toast from "react-hot-toast";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { packageSchema } from "@/schema/Schema";
-
+import axios from "axios";
 
 
 const PackageForm = ({ packageTitle }) => {
