f18de377cd86be831350adb8c8fe8796c73c3508
AmeerMavia97 <maviaking97@gmail.com>
2026-08-13 19:55:19 +0500

    Changes

---
+++ src/app/sitemap.js
@@ -0,0 +1,79 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://webdesignspectrum.com/",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 1,
+    },
+    {
+      url: "https://webdesignspectrum.com/wyoming-web-design",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 0.8,
+    },
+    {
+      url: "https://webdesignspectrum.com/cms-website-development",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 0.8,
+    },
+    {
+      url: "https://webdesignspectrum.com/ecommerce-web-development",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 0.8,
+    },
+    {
+      url: "https://webdesignspectrum.com/web-application-development",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 0.8,
+    },
+    {
+      url: "https://webdesignspectrum.com/mobile-application-development",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 0.8,
+    },
+    {
+      url: "https://webdesignspectrum.com/logo-and-branding",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 0.8,
+    },
+    {
+      url: "https://webdesignspectrum.com/seo-friendly-web-design",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 0.8,
+    },
+    {
+      url: "https://webdesignspectrum.com/social-media-marketing-services-for-small-businesses",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 0.8,
+    },
+    {
+      url: "https://webdesignspectrum.com/packages",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 0.8,
+    },
+    {
+      url: "https://webdesignspectrum.com/portfolio",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 0.8,
+    },
+    {
+      url: "https://webdesignspectrum.com/about-us",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 0.8,
+    },
+    {
+      url: "https://webdesignspectrum.com/contact",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 0.8,
+    },
+    {
+      url: "https://webdesignspectrum.com/terms",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 0.8,
+    },
+    {
+      url: "https://webdesignspectrum.com/privacy",
+      lastModified: "2026-08-12T19:49:57+00:00",
+      priority: 0.8,
+    },
+  ];
+}
